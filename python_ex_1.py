e1490362ff3b901de058c7bbd12af8330ee31879
Prim <tombo201@gmail.com>
2024-11-06 19:13:12 -0500

    adding files

---
+++ python_ex_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+filename = 'shakespeare-hamlet.txt'
+with open(filename, encoding='utf-8') as f:
+    contents = f.read()
+    words = contents.split()
+    num_words = len(words)
+    print(f"the file {filename} has about {num_words} words.")
+
